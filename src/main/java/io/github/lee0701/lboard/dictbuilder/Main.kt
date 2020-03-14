5312016f80ad61f7f31d66f9255c10ff5bf9983b
Lee0701 <hgl1002@naver.com>
2020-03-14 17:53:18 +0900

    Add multi thread to dictionary building

--- src/main/java/io/github/lee0701/lboard/dictbuilder/Main.kt
+++ src/main/java/io/github/lee0701/lboard/dictbuilder/Main.kt
@@ -3,6 +3,10 @@ package io.github.lee0701.lboard.dictbuilder
 import org.openkoreantext.processor.OpenKoreanTextProcessorJava
 import java.io.*
 import java.text.Normalizer
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import kotlin.concurrent.thread
 import kotlin.math.log10
 
 fun main() {
@@ -77,30 +81,51 @@ fun writeBinaryDictionary(dictionary: TrieDictionary, outFileName: String) {
     TrieDictionary.write(dictionary.root.serialize(), out)
 }
 
-fun buildDictionaries(sourceFileName: String, dirName: String, cutFrequency: Int = 10) {
+fun buildDictionaries(sourceFileName: String, dirName: String, cutFrequency: Int = 10, threadCount: Int = 32) {
     val dirFile = File(dirName)
     dirFile.mkdirs()
 
     val br = BufferedReader(InputStreamReader(FileInputStream(sourceFileName)))
+    val queue = LinkedBlockingQueue<String>()
+
+    val executor = Executors.newCachedThreadPool()
 
     val dict = mutableMapOf<String, MutableMap<String, Int>>()
 
-    var i = 0
-    br.forEachLine { line ->
-        if(i % 1000 == 0) println(i)
-        if(line.isEmpty()) return@forEachLine
-//        val normalized = OpenKoreanTextProcessorJava.normalize(line)
-        val normalized = line
-        val tokens = OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(
-            OpenKoreanTextProcessorJava.tokenize(normalized))
-        tokens.forEach { token ->
-            val pos = token.pos.name
-            val text = token.text
-            if(!dict.containsKey(pos)) dict[pos] = mutableMapOf()
-            if(!dict[pos]!!.containsKey(text)) dict[pos]!![text] = 0
-            dict[pos]!![text] = dict[pos]!![text]!! + 1
+    var lines = 0
+
+    val readerThread = Thread {
+        br.forEachLine { line ->
+            queue.put(line)
         }
-        i++
+    }
+    readerThread.start()
+
+    val callables = (0 until threadCount).map { thread ->
+        Callable {
+            while(readerThread.isAlive || queue.isNotEmpty()) {
+                val line = queue.take()
+                if(line.isEmpty()) continue
+//                val normalized = OpenKoreanTextProcessorJava.normalize(line)
+                val normalized = line
+                val tokens = OpenKoreanTextProcessorJava.tokensToJavaKoreanTokenList(
+                    OpenKoreanTextProcessorJava.tokenize(normalized))
+                tokens.forEach { token ->
+                    val pos = token.pos.name
+                    val text = token.text
+                    if(!dict.containsKey(pos)) dict[pos] = mutableMapOf()
+                    if(dict[pos]?.containsKey(text) == false) dict[pos]?.set(text, 0)
+                    dict[pos]?.let { posDict -> posDict[text]?.let { posDict[text] = it + 1 } }
+                }
+                lines++
+                if(lines % 1000 == 0) println(lines)
+            }
+        }
+    }
+    val futures = executor.invokeAll(callables)
+
+    futures.forEach { future ->
+        future.get()
     }
 
     dict.forEach { pos, words ->
